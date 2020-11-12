6bfd26aee6a7b47c8e3fe4515460fe17d44983db
Adm <tarasklyushta@gmail.com>
2020-11-13 00:04:12 +0200

    Add basic profile page

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const debug = process.env.NODE_ENV !== "production";
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  assetPrefix: !debug ? "" : "",
+};
